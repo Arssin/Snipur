de8595483edf5eb0ac2819a9510717084d5d16e2
Arssin <mateusz.binieda@onet.pl>
2022-07-01 00:25:48 +0200

    Setting up window

--- project.py
+++ project.py
@@ -1,1 +1,14 @@
+import pygame , sys
+
 print('hello world')
+print(pygame.__version__)
+
+resolution = (1280,720)
+
+screen = pygame.display.set_mode(resolution)
+print(screen)
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit(0)
