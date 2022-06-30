b2fb0093eccb56952c0b4bdea2e2390149f40c1f
Arssin <mateusz.binieda@onet.pl>
2022-07-01 00:53:14 +0200

    add viewfinder

--- project.py
+++ project.py
@@ -6,9 +6,18 @@ print(pygame.__version__)
 resolution = (1280,720)
 
 screen = pygame.display.set_mode(resolution)
-print(screen)
+
+radius = 40
 
 while True:
+  # Handle events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit(0)
+
+  screen.fill('white')
+  mx,my = pygame.mouse.get_pos()
+  # Draw
+  circle1 = pygame.draw.circle(screen, "blue", (200,100), radius, 0 )
+  circle2 = pygame.draw.circle(screen, "red", (mx,my), radius,5 )
+  pygame.display.flip()
