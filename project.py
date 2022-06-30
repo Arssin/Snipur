08d43ee31c261d1f52876ccb58b414d3a768534b
Arssin <mateusz.binieda@onet.pl>
2022-07-01 01:05:37 +0200

    Add speed for circle following

--- project.py
+++ project.py
@@ -10,8 +10,8 @@ resolution = (1280,720)
 screen = pygame.display.set_mode(resolution)
 
 radius = 40
-x = 200
-y = 100
+x = 10
+y = 20
 
 while True:
   # Handle events
@@ -25,6 +25,7 @@ while True:
   circle1 = pygame.draw.circle(screen, "blue", (x,y), radius, 0 )
   circle2 = pygame.draw.circle(screen, "red", (mx,my), radius,5 )
 
+ # To which point is going
   dx = mx - x
   dy = my - y
 
@@ -32,7 +33,7 @@ while True:
   movex = math.sin(angle) 
   movey = math.cos(angle)
 
-  x += movex
-  y += movey
+  x += movex * 0.2
+  y += movey * 0.2
 
   pygame.display.flip()
