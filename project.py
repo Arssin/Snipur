cf15cbd06175d353448d37fd2081f3a9a8edd3ba
Arssin <mateusz.binieda@onet.pl>
2022-07-01 15:34:39 +0200

    add class

--- project.py
+++ project.py
@@ -1,36 +1,65 @@
+# -------------- Package + Init -----------------
+
 import random
 import pygame , sys
-
-
 pygame.init()
+
+# -------------- Settings -----------------
+
 resolution = (1280,720)
 screen = pygame.display.set_mode(resolution)
-
 ms_delay = 3000
 spawn_circle_event = pygame.USEREVENT + 1
 pygame.time.set_timer(spawn_circle_event, ms_delay)
-
 # Circles values
 radiuscrosshair = 25
 radiuscircles = 15
-# x = 100
-# y = 200
-
 # Clock
 clock = pygame.time.Clock()
 delta = 0.0
-
 #Max Tick Per second
 max_tps = 60.0
-
 #Points
 points = 0
-
+game_over = False
 all_targets = []
+
+#COLORS
+BLUE = (000,000,255)
 
 circle1 = pygame.draw.circle(screen, "blue", (random.randint(0,1280),random.randint(0,720)), radiuscircles, 0 )
 
-while True:
+# -------------- Classes -----------------
+
+class Circles:
+  def __init__(self,x,y,radius=radiuscircles, color=BLUE, thick=0):
+    self.x = x
+    self.y = y
+    self.radius = radius
+    self.color = color
+    self.thick = thick
+    self.hitbox = (x,y,radius)
+    self.count = 0
+
+  def draw(self,screen):
+    self.hitbox= ()
+    if self.count >= 8:
+      self.count = 0
+    screen.blit(circle1,self.x,self.y)
+    self.count += 1
+    pygame.draw.circle(screen, "blue", self.hitbox, self.thick) 
+
+
+
+
+# -------------- Functions -----------------
+
+
+while not game_over:
+
+  # for e in all_targets:
+  #   e.draw(screen)
+
   # Handle events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -39,10 +68,14 @@ while True:
     if event.type == spawn_circle_event: 
       print('Hello')
 
+
+
+
     if pygame.mouse.get_pressed()[0]:
       print('Mouse pressed')
 
-
+  for e in all_targets:
+    e.draw(screen)
 
   #Border
   screen.fill('white')
