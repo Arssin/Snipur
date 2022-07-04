d5cfc90cab3031c1057c0413bdc92a9205fe6eb6
Arssin <mateusz.binieda@onet.pl>
2022-07-04 15:53:56 +0200

    features

--- project.py
+++ project.py
@@ -14,7 +14,7 @@ pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
 
 margin = 100
-bar = 100
+lowerMargin = 100
 score = 0
 
 font = pygame.font.SysFont("Arial", 25)
@@ -28,93 +28,105 @@ lightGreen = (25, 111, 61)
 purple = (155, 89, 182)
 
 
-
-
-
 # Circles values
 radiuscrosshair = 25
 radiuscircles = 15
 circlesX = 0
 circlesY = 30
-# Clock
-#Max Tick Per second
+
 max_tps = 60.0
-#Points
-points = 0
 game_over = False
-
-
-hit = 0
-count = 0
-
 
 # -------------- Classes  -----------------
 class Circle:
     def __init__(self):
-         self.a = random.randint(30, 40)
-         self.b = self.a + random.randint(0, 10)
-         self.x = random.randrange(margin, width - self.a - margin)
-         self.pos = [random.randint(0, 1280), random.randint(0, 720)]
-         self.color = blue
+         self.x = random.randint(0,1280)
+         self.y = random.randint(0,720)
          self.radius = 15
          self.color = random.choice([blue,red,purple,lightGreen,])
 
     def draw(self):
-         pygame.draw.circle(screen, self.color, (self.pos[0], self.pos[1]), self.radius)
+          pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
 
+    def hit(self):
+          global score
+          pos = pygame.mouse.get_pos()
+
+          # if hitCircle(self.x, self.y):
+          #   score += 1
+          #   self.reset()
+
+    def reset(self): 
+         self.x = random.randint(0,1280)
+         self.y = random.randint(0,720)
+         self.radius = 15
+         self.color = random.choice([blue,red,purple,lightGreen,])
     
+    
+circles = []
+noCircles = 10
 
+for i in range(noCircles):
+    obj = Circle()
+    circles.append(obj)
+
+# def hitCircle(x,y,a,b,pos):
+#     if (x < pos[0] < x + a) and (y < pos[1] < y + b):
+#         return True
+#     else:
+#         return False
 
 def pointer():
-  mx,my = pygame.mouse.get_pos()
-  pygame.draw.circle(screen, red, (mx,my), radiuscrosshair, 5 )
+  pos = pygame.mouse.get_pos()
+  r = 25
+  l = 20
+  color = lightGreen
+  # for i in range(noCircles):
+  #   if hitCircle(circles[i].x, circles[i].y, circles[i].a, circles[i].b, pos):
+  #     color = red
+  pygame.draw.circle(screen, color,  (pos[0] - r/2, pos[1] - r/2), radiuscrosshair, 5 )
 
+def upperScore():
+    pygame.draw.rect(screen, purple, (0, height - lowerMargin, width, lowerMargin))
 
-#Random generator 
-targets = []
-noTargets = 10
-number = 5
-ox = random.randint(0,1280)
-oy = random.randint(0,720)
+def scoreShow():
+    scoreText = font.render("Balloons Bursted : " + str(score), True, white)
+    screen.blit(scoreText, (150, height - lowerMargin + 50))
 
+def close():
+    pygame.quit()
+    sys.exit()
 
+def game():
+    global score
+    loop = True
 
+    while loop:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                close()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    close()
+                if event.key == pygame.K_r:
+                    score = 0
+                    game()
 
-# -------------- GameLoop -----------------
-clock = pygame.time.Clock()
-game_over = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for i in range(noCircles):
+                    circles[i].hit()
 
-while not game_over:
+        screen.fill(white)
+        
+        for i in range(noCircles):
+            circles[i].draw()
 
+        pointer()
+        
+        
+        upperScore()
+        scoreShow()
+        pygame.display.update()
+        clock.tick(60)
 
-
-  # Handle events
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      sys.exit(0)
-
-
-    if pygame.mouse.get_pressed()[0]:
-      print('Mouse pressed')
-
-
-  #Border
-  screen.fill('white')
-
-  # Draw circle and mouse position
-
-  
-    
-
-
-  pointer()
-
-
-
-
-  
-
-  pygame.display.update()
-  clock.tick(60)
-
-pygame.quit()
+game()
